79a762b4c0d82f5065c2e283af0198bdee9a96e7
gitForDavidL <davidtach26@gmail.com>
2023-10-29 22:11:25 -0500

    added libraries

--- pagina.py
+++ pagina.py
@@ -6,10 +6,6 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 
-pip install pandas-datareader
-pip install plotly
-pip install seaborn
-
 
 st.title('Proyecto Big Data')
 st.write('### Susana Valencia')
